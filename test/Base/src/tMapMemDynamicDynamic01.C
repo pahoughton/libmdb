31cbac76a927cbc022ec4eacbb680eaef228e60b
houghton <>
1997-03-13 02:43:48 +0000

    Added some tests.

--- test/Base/src/tMapMemDynamicDynamic01.C
+++ test/Base/src/tMapMemDynamicDynamic01.C
@@ -55,59 +55,69 @@ tMapMemDynamicDynamic( void )
     t.dumpFreeList( cout ) << endl;
     t.dumpNodes( cout ) << endl;
 
-    for( vector<int>::iterator them = TestNodeSize.begin();
-	 them != TestNodeSize.end();
-	 ++ them )
-      {
-	nodes.push_back( Node( t.getMem( (*them) ), (*them) ) );
-	
-	cout << t.dump();
-      }
+    {
+      int cnt = 0;
+      for( vector<int>::iterator them = TestNodeSize.begin();
+	   them != TestNodeSize.end();
+	   ++ them )
+	{
+	  cout << "Getting: " << cnt++ << " size: " << (*them) << endl;
+	  
+	  nodes.push_back( Node( t.getMem( (*them) ), (*them) ) );
+	  
+	  cout << t.dump() << endl;;
+	}
+    }
     
     t.dumpFreeList( cout ) << endl;
     t.dumpNodes( cout ) << endl;
-    
-    for( NodeList::iterator them = nodes.begin();
-	 them != nodes.end();
-	 ++ them )
-      {
-	cout << "Alloced Nodes: " << setw(6) << (*them).offset
-	     << setw(8) << (*them).size
-	     << endl;
-      }
+
+    {
+      for( NodeList::iterator them = nodes.begin();
+	   them != nodes.end();
+	   ++ them )
+	{
+	  cout << "Alloced Nodes: " << setw(6) << (*them).offset
+	       << setw(8) << (*them).size
+	       << endl;
+	}
+    }
     
     random_shuffle( TestIndex.begin(), TestIndex.end() );
 
     cout << t.dump() << endl;
     t.dumpNodes( cout );
     
-    int junk;
-    cin >> junk;
+    //    int junk;
+    //    cin >> junk;
   }
 
   {
     cout << "\n*** REOPEN ***" << endl;
-    int junk;
-    cin >> junk;
+
+    //    int junk;
+    //    cin >> junk;
     
     MapMemDynamicDynamic	t( "../data/mmdd.map",
 				   (ios::open_mode)(ios::in | ios::out) );
 
     cout << t.dump() << endl;
     t.dumpNodes( cout );
-    
-    for( vector<int>::iterator them = TestIndex.begin();
-	 them != TestIndex.end();
-	 ++ them )
-      {
-	cout << "\n *** Freeing: " << (*them)
-	     << " offset: " << nodes[ (*them) ].offset
-	     << " size: " << nodes[ (*them) ].size
-	     << endl;
-	
-	t.freeMem( nodes[ (*them) ].offset );
-	t.dumpNodes( cout );
-      }
+
+    {
+      for( vector<int>::iterator them = TestIndex.begin();
+	   them != TestIndex.end();
+	   ++ them )
+	{
+	  cout << "\n *** Freeing: " << (*them)
+	       << " offset: " << nodes[ (*them) ].offset
+	       << " size: " << nodes[ (*them) ].size
+	       << endl;
+	  
+	  t.freeMem( nodes[ (*them) ].offset );
+	  t.dumpNodes( cout );
+	}
+    }
     
     cout << t.dump() << endl;
     t.dumpFreeList( cout );
@@ -119,6 +129,9 @@ tMapMemDynamicDynamic( void )
 // Revision Log:
 //
 // $Log$
+// Revision 2.2  1997/03/13 02:43:48  houghton
+// Added some tests.
+//
 // Revision 2.1  1997/03/08 10:30:26  houghton
 // Initial Version.
 //
