7dd5df1cf19364f5eec6dc6cf269c731d25913e5
houghton <>
1997-03-19 16:23:51 +0000

    Reworked test.

--- test/Base/src/tMapMemDynamicDynamic01.C
+++ test/Base/src/tMapMemDynamicDynamic01.C
@@ -19,6 +19,7 @@
 #include <algorithm>
 #include <vector>
 #include <iomanip>
+#include <cstring>
 
 struct Node
 {
@@ -36,9 +37,9 @@ tMapMemDynamicDynamic( void )
   vector< int > TestIndex;
   
   {
-    for( int i = 0; i < 10 ; i++ )
+    for( int i = 0; i < 20 ; i++ )
       {
-	TestNodeSize.push_back( rand() % 2000 );
+	TestNodeSize.push_back( 40 ); //50 + (rand() % 20) );
 	TestIndex.push_back( i );
       }
       
@@ -61,16 +62,92 @@ tMapMemDynamicDynamic( void )
 	   them != TestNodeSize.end();
 	   ++ them )
 	{
-	  cout << "Getting: " << cnt++ << " size: " << (*them) << endl;
+	  off_t offset = t.getMem( *them );
+
+	  cout << "GETMEM: " <<  cnt++ << setw(4)
+	       << (*them) << setw(8) << offset << endl;
+	  t.dumpNodes( cout ) << endl;
+
+	  memset( t.getAddr( offset ), 0xff, *them );
 	  
-	  nodes.push_back( Node( t.getMem( (*them) ), (*them) ) );
+	  nodes.push_back( Node( offset, (*them) ) );
 	  
-	  cout << t.dump() << endl;;
 	}
     }
+
+    int ni = 0;
+
+    for( ; ni < 5; ++ ni )
+	{
+	  t.freeMem( nodes[ni].offset );
+	  cout << "FREEMEM: " << nodes[ni].offset << endl;
+	  t.dumpNodes( cout ) << endl;
+	}
+
     
-    t.dumpFreeList( cout ) << endl;
+    t.freeMem( nodes[ni + 3].offset );
+    cout << "FREEMEM: " << nodes[ni].offset << endl;
     t.dumpNodes( cout ) << endl;
+    
+    t.freeMem( nodes[ni + 1].offset );
+    cout << "FREEMEM: " << nodes[ni].offset << endl;
+    t.dumpNodes( cout ) << endl;
+    
+    t.freeMem( nodes[ni + 2].offset );
+    cout << "FREEMEM: " << nodes[ni].offset << endl;
+    t.dumpNodes( cout ) << endl;
+    
+    t.freeMem( nodes[ni].offset );
+    cout << "FREEMEM: " << nodes[ni].offset << endl;
+    t.dumpNodes( cout ) << endl;
+
+    ni += 4;
+    
+    {
+      int cnt = 0;
+      for( vector<int>::iterator them = TestNodeSize.begin();
+	   them != TestNodeSize.end();
+	   ++ them )
+	{
+	  off_t offset = t.getMem( *them );
+
+	  cout << "GETMEM: " << cnt++ << setw(4) << (*them) << setw(8) << offset << endl;
+	  t.dumpNodes( cout ) << endl;
+
+	  memset( t.getAddr( offset ), 0xff, *them );
+	  
+	  nodes.push_back( Node( offset, (*them) ) );
+	  
+	}
+    }
+	  
+    for( ; ni < 15; ++ ni )
+	{
+	  t.freeMem( nodes[ni].offset );
+	  cout << "FREEMEM: " << nodes[ni].offset << endl;
+	  t.dumpNodes( cout ) << endl;
+	}
+
+    {
+      int cnt = 0;
+      for( vector<int>::iterator them = TestNodeSize.begin();
+	   them != TestNodeSize.end();
+	   ++ them )
+	{
+	  off_t offset = t.getMem( *them );
+
+	  cout << "GETMEM: " << cnt++ << setw(4) << (*them) << setw(8) << offset << endl;
+	  t.dumpNodes( cout ) << endl;
+
+	  memset( t.getAddr( offset ), 0xff, *them );
+	  
+	  nodes.push_back( Node( offset, (*them) ) );
+	  
+	}
+    }
+	  
+  }
+#if 0
 
     {
       for( NodeList::iterator them = nodes.begin();
@@ -122,6 +199,7 @@ tMapMemDynamicDynamic( void )
     cout << t.dump() << endl;
     t.dumpFreeList( cout );
   }
+#endif
   return( true );
 }
 
@@ -129,6 +207,9 @@ tMapMemDynamicDynamic( void )
 // Revision Log:
 //
 // $Log$
+// Revision 2.3  1997/03/19 16:23:51  houghton
+// Reworked test.
+//
 // Revision 2.2  1997/03/13 02:43:48  houghton
 // Added some tests.
 //
