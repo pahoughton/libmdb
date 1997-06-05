971a95dcb8582f1e3e6f8548f366f4291b9a2ad3
houghton <>
1997-06-05 11:22:51 +0000

    Cleanup. Change to be part of libMdb (vs Clue1). Added type MapVersion. Added constructor that can create or open existing. Changed to use new MapFile types. Added createMapMem and openMapMemMehtods. Moved inlines to MapMem.ii

--- src/Mdb/MapMem.hh
+++ src/Mdb/MapMem.hh
@@ -2,16 +2,242 @@
 #define _MapMem_hh_
 //
 // File:        MapMem.hh
+// Project:	Mdb
 // Desc:        
-//              
 //
-// Author:      Paul Houghton - (houghton@cworld.wiltel.com)
+//
+//
+// Quick Start: - short example of class usage
+//
+// Notes:
+//
+//  This class was originally part of libClue1
+//
+// Author:      Paul A. Houghton - (paul.houghton@wcom.com)
 // Created:     11/18/94 10:06
 //
-// Revision History:
+// Revision History: (See end of file for Revision Log)
 //
-// 
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+//  $Id$
+//
+
+#include <MdbConfig.hh>
+#include <MapFile.hh>
+#include <DumpInfo.hh>
+#include <iostream>
+
+#if defined( MDB_DEBUG )
+#define inline
+#endif
+
+class MapMem : public MapFile
+{
+
+public:
+
+  typedef unsigned long	    MapVersion;
+  
+  enum MapType
+  {
+    MM_FIXED,	    // fixed 'map to' address
+    MM_DYNAMIC,	    // dynamic 'map to' address
+    MM_UNDEFINED
+  };
+
+  enum MapError
+  {
+    E_OK,
+    E_MAPFILE,
+    E_FILESIZE,
+    E_VERSION,
+    E_BADTYPE,
+    E_UNDEFINED
+  };
+
+  // use this to create an new map or open and existing one.
+  MapMem( const char *	    fileName,
+	  MapType	    type,
+	  MapVersion	    version,
+	  ios::open_mode    mode,
+	  bool		    create,
+	  MapAddr	    baseAddr,
+	  size_type	    size,
+	  MapMask	    permMask = 0777 );
+	  
+	  
+  // use this to create a new map
+  MapMem( const char * 	    fileName,
+	  MapAddr    	    baseAddr,
+	  MapType   	    type,
+	  MapVersion	    version,
+	  size_type	    size,
+	  MapMask	    permMask = 0777 );
+
+  // use this to open an existing map
+  MapMem( const char * 	    fileName,
+	  MapType   	    type,
+	  MapVersion	    version,
+	  ios::open_mode    mode = ios::in );
+
+  virtual ~MapMem( void );
+  
+  MapVersion	    getMapVersion( void ) const;
+  void *    	    getMapToAddr( void ) const;
+  MapType   	    getType( void ) const;
+  const char *	    getTypeName( void ) const;
+  size_type	    getMapSize( void ) const;
+  
+  virtual bool	    	good( void ) const;
+  virtual const char * 	error( void ) const;
+  virtual const char *	getClassName( void ) const;
+  virtual const char *  getVersion( bool withPrjVer = true ) const;
+  virtual ostream &     dumpInfo( ostream &	dest = cerr,
+				  const char *  prefix = "    ",
+                                  bool          showVer = true ) const;
+
+  static const ClassVersion version;
+
+  inline
+  DumpInfo< MapMem >	    dump( const char *	prefix = "    ",
+				  bool		showVer = true ) const;
+  
+protected:
+
+  struct MapInfo
+  {
+    MapType 	    type;
+    MapVersion	    version;
+    unsigned long   base;
+    unsigned long   size;
+  };
+
+private:
+
+  MapMem( const MapMem & copyFrom );
+  MapMem & operator=( const MapMem & assignFrom );
+
+  void	createMapMem( MapType type, MapVersion version, MapAddr baseAddr );
+  void	openMapMem( const char * fileName,
+		    MapType	    type,
+		    MapVersion	    version,
+		    ios::open_mode  mode );
+  
+  inline const MapInfo * 	getMapInfo( void ) const;
+  inline MapInfo *		getMapInfo( void );
+  
+  static const char * ErrorStrings[];
+  static const char * TypeStrings[];
+  
+  MapError  	mapError;
+  int	    	osErrno;
+};
+
+
+#if !defined( inline )
+#include <MapMem.ii>
+#else
+#undef inline
+#endif
+
+
+
+//
+// Detail Documentation
+//
+//  Data Types: - data types defined by this header
+//
+//  	MapMem	class
+//
+//  Constructors:
+//
+//  	MapMem( );
+//
+//  Destructors:
+//
+//  Public Interface:
+//
+//	virtual ostream &
+//	write( ostream & dest ) const;
+//	    write the data for this class in binary form to the ostream.
+//
+//	virtual istream &
+//	read( istream & src );
+//	    read the data in binary form from the istream. It is
+//	    assumed it stream is correctly posistioned and the data
+//	    was written to the istream with 'write( ostream & )'
+//
+//	virtual ostream &
+//	toStream( ostream & dest ) const;
+//	    output class as a string to dest (used by operator <<)
+//
+//	virtual istream &
+//	fromStream( istream & src );
+//	    Set this class be reading a string representation from
+//	    src. Returns src.
+//
+//  	virtual Bool
+//  	good( void ) const;
+//  	    Return true if there are no detected errors associated
+//  	    with this class, otherwise false.
+//
+//  	virtual const char *
+//  	error( void ) const;
+//  	    Return a string description of the state of the class.
+//
+//  	virtual const char *
+//  	getClassName( void ) const;
+//  	    Return the name of this class (i.e. MapMem )
+//
+//  	virtual const char *
+//  	getVersion( bool withPrjVer = true ) const;
+//  	    Return the version string of this class.
+//
+//	virtual ostream &
+//	dumpInfo( ostream & dest, const char * prefix, bool showVer );
+//	    output detail info to dest. Includes instance variable
+//	    values, state info & version info.
+//
+//	static const ClassVersion version
+//	    Class and project version information. (see ClassVersion.hh)
+//
+//  Protected Interface:
+//
+//  Private Methods:
+//
+//  Associated Functions:
+//
+//  	ostream &
+//  	operator <<( ostream & dest, const MapMem & src );
+//
+//	istream &
+//	operator >> ( istream & src, MapMem & dest );
+//
+// Example:
+//
+// See Also:
+//
+// Files:
+//
+// Documented Ver:
+//
+// Tested Ver:
+//
+// Revision Log:
+//
 // $Log$
+// Revision 2.5  1997/06/05 11:22:51  houghton
+// Cleanup.
+// Change to be part of libMdb (vs Clue1).
+// Added type MapVersion.
+// Added constructor that can create or open existing.
+// Changed to use new MapFile types.
+// Added createMapMem and openMapMemMehtods.
+// Moved inlines to MapMem.ii
+//
 // Revision 2.4  1997/04/04 20:49:33  houghton
 // Cleanup.
 //
@@ -31,131 +257,7 @@
 // New Style Avl an memory management. Many New Classes
 //
 //
-
-#include <ClueConfig.hh>
-
-#include <MapFile.hh>
-  
-class MapMem : public MapFile
-{
-
-public:
-
-  enum MapType
-  {
-    MM_FIXED,
-    MM_DYNAMIC,
-    MM_UNDEFINED
-  };
-
-  enum MapError
-  {
-    E_OK,
-    E_OSERROR,
-    E_MAPFILE,
-    E_BADFILE,
-    E_BADVER,
-    E_BADTYPE,
-    E_UNDEFINED
-  };
-
-  struct MapInfo
-  {
-    MapType 	    type;
-    unsigned long   version;
-    unsigned long   base;
-    unsigned long   size;
-  };
-
-  // use this to create a new map
-  MapMem( const char * 	    fileName,
-	  caddr_t    	    baseAddr,
-	  MapType   	    type,
-	  unsigned long	    version,
-	  unsigned long	    size,
-	  unsigned short    permMask = 0 );
-
-  // use this to open an existing map
-  MapMem( const char * 	    fileName,
-	  MapType   	    type,
-	  unsigned long	    version,
-	  ios::open_mode    mode = ios::in );
-
-  virtual ~MapMem( void );
-  
-  unsigned long	    getVersion( void ) const;
-  void *    	    getMapToAddr( void ) const;
-  MapType   	    getType( void ) const;
-  const char *	    getTypeName( void ) const;
-  unsigned long	    getMapSize( void ) const;
-
-  virtual ostream & 	getStats( ostream & dest ) const;
-
-  virtual const char *	getClassName( void ) const { return "MapMem"; };
-  virtual bool	    	good( void ) const;
-  virtual const char * 	error( void ) const;
-
-  virtual ostream &	dumpInfo( ostream &	dest,
-				  const char *	prefix = "    ",
-				  bool		showVer = false ) const;
-  
-  friend inline ostream & operator<<( ostream & dest, const MapMem & mm );
-  
-protected:
-
-  MapInfo * 	getMapInfo( void );
-  
-private:
-
-  static const char * ErrorStrings[];
-  static const char * TypeStrings[];
-  
-  MapMem( const MapMem & copyFrom );
-  MapMem & operator=( const MapMem & assignFrom );
-
-  const char *  mapFileName;
-  MapInfo * 	mapInfo;
-
-  int	    	osErrno;
-  MapError  	mapError;
-};
-
-
-//
-// Inline methods
-//
-
-inline
-void *
-MapMem::getMapToAddr( void ) const
-{
-  return( (mapInfo) ? (void*)mapInfo->base : 0 );
-}
-
-inline
-unsigned long
-MapMem::getMapSize( void ) const
-{
-  return( (mapInfo) ? mapInfo->size : 0 );
-}
-
-
-inline
-MapMem::MapInfo *
-MapMem::getMapInfo( void )
-{
-  mapInfo = (MapInfo *)MapFile::getBase();
-  return( mapInfo );
-}
-
-inline
-ostream &
-operator<<( ostream & dest, const MapMem & mm )
-{
-  return( mm.getStats( dest ) );
-}
-
-#endif // ! def _MapMem_hh_ 
+// Copyright:
 //
 //              This software is the sole property of
 // 
@@ -167,3 +269,5 @@ operator<<( ostream & dest, const MapMem & mm )
 //                      All Rights Reserved.  
 // 
 //
+#endif // ! def _MapMem_hh_ 
+
