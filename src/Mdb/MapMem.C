7ab5476a19286e83ee61a5e0098c6085521bf71a
houghton <>
1997-06-05 11:23:36 +0000

    Cleanup. Change to be part of libMdb (vs Clue1). Added constructor that can create or open existing. Changed to use new MapFile types. Added createMapMem and openMapMemMehtods.

--- src/Mdb/MapMem.C
+++ src/Mdb/MapMem.C
@@ -1,35 +1,19 @@
 //
 // File:        MapMem.C
+// Project:	Mdb
 // Desc:        
-//              
 //
-// Author:      Paul Houghton - (houghton@cworld.wiltel.com)
+//  Compiled sources for MapMem
+//  
+// Author:      Paul A. Houghton - (paul.houghton@wcom.com)
 // Created:     11/21/94 10:59 
 //
-// Revision History:
+// Revision History: (See end of file for Revision Log)
 //
-// $Log$
-// Revision 2.4  1997/04/04 20:49:11  houghton
-// Cleanup.
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
 //
-// Revision 2.3  1997/03/07 11:49:12  houghton
-// Add dumpInfo.
-//
-// Revision 2.2  1997/03/03 14:32:31  houghton
-// Added virtual destructor.
-//
-// Revision 2.1  1995/11/10 12:42:29  houghton
-// Change to Version 2
-//
-// Revision 1.4  1995/11/05  16:32:33  houghton
-// Revised
-//
-// Revision 1.1  1995/02/13  16:08:49  houghton
-// New Style Avl an memory management. Many New Classes
-//
-//
-static const char * RcsId =
-"$Id$";
 
 #include "MapMem.hh"
 #include "Str.hh"
@@ -38,14 +22,22 @@ static const char * RcsId =
 #include <fcntl.h>
 #endif
 
+#if defined( MDB_DEBUG )
+#include "MapMem.ii"
+#endif
+
+MDB_VERSION(
+  MapMem,
+  "$Id$");
+
+
 const char * MapMem::ErrorStrings[] =
 {
-  ": Ok",
-  ": OS - ",
+  ": ok",
   ": ",
-  ": Bad file",
-  ": Bad version",
-  ": Bad map type",
+  ": file to small",
+  ": incorect version",
+  ": map type mismatch",
   0
 };
 
@@ -58,35 +50,43 @@ const char * MapMem::TypeStrings[] =
 
 
 //
+// Constructor for creating a new map or opening an existing one.
+//
+MapMem::MapMem(
+  const char *	    fileName,
+  MapType	    type,
+  MapVersion	    version,
+  ios::open_mode    mode,
+  bool		    create,
+  MapAddr	    baseAddr,
+  size_type	    size,
+  MapMask	    permMask
+  )
+  : MapFile( fileName, baseAddr, mode, create, size, permMask ),
+    mapError( E_OK ),
+    osErrno( 0 )
+{
+  if( create )
+    createMapMem( type, version, baseAddr );
+  else
+    openMapMem( fileName, type, version, mode );
+}
+
+  
+//
 // Constructor for creating a new map
 //
 MapMem::MapMem(
   const char * 	    fileName,
-  caddr_t	    baseAddr,
+  MapAddr	    baseAddr,
   MapType   	    type,
-  unsigned long	    version,
-  unsigned long	    size,
-  unsigned short    permMask
+  MapVersion	    version,
+  size_type	    size,
+  MapMask	    permMask
   )
   : MapFile( fileName, size, baseAddr, permMask )
 {
-  osErrno = 0;
-  mapFileName = fileName;
-  
-  mapInfo = (MapInfo *)MapFile::getBase();
-  
-  if( mapInfo != 0 )
-    {
-      mapInfo->type     = type;
-      mapInfo->version  = version;
-      mapInfo->base	= (unsigned long)baseAddr;      
-      mapInfo->size	= getSize();
-      mapError = E_OK;
-    }
-  else
-    {
-      mapError = E_MAPFILE;
-    }
+  createMapMem( type, version, baseAddr );
 }
 
 
@@ -94,59 +94,12 @@ MapMem::MapMem(
 MapMem::MapMem(
   const char *	    fileName,
   MapType   	    type,
-  unsigned long	    version,
+  MapVersion	    version,
   ios::open_mode    mode
   )
+  : MapFile( fileName, 0, mode )
 {
-  mapError = E_OK;
-
-  mapFileName = fileName;
-  
-  int fd = open( fileName, O_RDONLY, 0 );
-  
-  if( fd == -1 )
-    {
-      mapError = E_OSERROR;
-      osErrno = errno;
-      return;
-    }
-
-  MapInfo   info;
-
-  errno = 0;
-  
-  if( read( fd, &info, sizeof( info ) ) != sizeof( info ) )
-    {
-      if( errno != 0 )
-	{
-	  mapError = E_OSERROR;
-	  osErrno = errno;
-	}
-      else
-	{
-	  mapError = E_BADFILE;
-	}      
-      return;
-    }
-
-
-  close( fd );
-  
-  if( info.type != type )
-    {
-      mapError = E_BADTYPE;
-      return;      
-    }
-
-  if( info.version != version )
-    {
-      mapError = E_BADVER;
-      return;
-    }
-
-  map( fileName, (caddr_t)info.base, mode );
-
-  mapInfo = (MapInfo *)MapFile::getBase();
+  openMapMem( fileName, type, version, mode );
 }
 
 MapMem::~MapMem( void )
@@ -154,15 +107,15 @@ MapMem::~MapMem( void )
 }
 
 unsigned long
-MapMem::getVersion( void ) const
+MapMem::getMapVersion( void ) const
 {
-  return( (mapInfo) ? mapInfo->version : 0 );
+  return( (getMapInfo()) ? getMapInfo()->version : 0 );
 }
 	
 MapMem::MapType
 MapMem::getType( void  ) const
 {
-  return( (mapInfo) ? mapInfo->type : MM_UNDEFINED );
+  return( (getMapInfo()) ? getMapInfo()->type : MM_UNDEFINED );
 }
 
 const char *
@@ -194,12 +147,6 @@ MapMem::error( void ) const
     {
       int errCnt = 0;
       
-      if( mapError == E_OSERROR )
-	{
-	  errCnt++;
-	  errStr << ": OS Error - " << mapFileName << " - " << strerror( osErrno );
-	}
-      
       if( mapError > E_MAPFILE && mapError < E_UNDEFINED )
 	{
 	  errCnt++;
@@ -220,27 +167,16 @@ MapMem::error( void ) const
   return( errStr.cstr() );
 }
 
-
-ostream &
-MapMem::getStats( ostream & dest ) const
+const char *
+MapMem::getClassName( void ) const
 {
-  dest << MapMem::getClassName() << ": stats" << '\n'
-       << "    Status:   " << error() << '\n'
-    ;
-  if( mapInfo != 0 )
-    {
-      dest << "    Map Ver:  " << getVersion() << '\n'
-	   << "    Type:     " << getTypeName() << '\n'
-	   << "    Map to:   " << getMapToAddr() << '\n'
-	   << "    Map Size: " << getMapSize() << endl
-	;
-    }
-  else
-    {
-      dest << "    No Map info available!" << endl;
-    }
+  return( "MapMem" );
+}
 
-  return( MapFile::getStats( dest ) );
+const char *
+MapMem::getVersion( bool withPrjVer ) const
+{
+  return( version.getVer( withPrjVer ) );
 }
   
 ostream &
@@ -252,7 +188,7 @@ MapMem::dumpInfo(
 {
   if( showVer )
     dest << MapMem::getClassName() << ":\n"
-	 << RcsId << '\n';
+	 << MapMem::getVersion() << '\n';
       
   if( ! MapMem::good() )
     dest << prefix << "Error: " << MapMem::error() << '\n';
@@ -265,7 +201,7 @@ MapMem::dumpInfo(
 
   MapFile::dumpInfo( dest, pre, false );
 
-  if( mapInfo != 0 )
+  if( getMapInfo() != 0 )
     {
       dest << prefix << "map Ver:  " << getVersion() << '\n'
 	   << prefix << "type:     " << getTypeName() << '\n'
@@ -281,6 +217,94 @@ MapMem::dumpInfo(
   return( dest );
 }
 
+void
+MapMem::createMapMem( MapType type, MapVersion version, MapAddr baseAddr )
+{
+  osErrno = 0;
+  
+  if( getMapInfo() != 0 )
+    {
+      getMapInfo()->type	= type;
+      getMapInfo()->version	= version;
+      getMapInfo()->base	= (unsigned long)baseAddr;      
+      getMapInfo()->size	= getSize();
+      mapError = E_OK;
+    }
+  else
+    {
+      mapError = E_MAPFILE;
+    }
+}
+
+void
+MapMem::openMapMem(
+  const char *	    fileName,
+  MapType   	    type,
+  MapVersion	    version,
+  ios::open_mode    mode
+  )
+{
+  if( ! MapFile::good() )
+    return;
+
+  if( getSize() < sizeof( MapInfo ) )
+    {
+      mapError = E_FILESIZE;
+      return;
+    }
+
+  const MapInfo * info = getMapInfo();
+
+  if( ! info )
+    return;
+
+  if( info->type != type )
+    {
+      mapError = E_BADTYPE;
+      return;
+    }
+
+  if( info->version != version )
+    {
+      mapError = E_VERSION;
+      return;
+    }
+
+  unmap();
+  map( fileName, (caddr_t)info->base, mode );
+      
+  mapError = E_OK;
+
+}
+
+// Revision Log:
+//
+// $Log$
+// Revision 2.5  1997/06/05 11:23:36  houghton
+// Cleanup.
+// Change to be part of libMdb (vs Clue1).
+// Added constructor that can create or open existing.
+// Changed to use new MapFile types.
+// Added createMapMem and openMapMemMehtods.
+//
+// Revision 2.4  1997/04/04 20:49:11  houghton
+// Cleanup.
+//
+// Revision 2.3  1997/03/07 11:49:12  houghton
+// Add dumpInfo.
+//
+// Revision 2.2  1997/03/03 14:32:31  houghton
+// Added virtual destructor.
+//
+// Revision 2.1  1995/11/10 12:42:29  houghton
+// Change to Version 2
+//
+// Revision 1.4  1995/11/05  16:32:33  houghton
+// Revised
+//
+// Revision 1.1  1995/02/13  16:08:49  houghton
+// New Style Avl an memory management. Many New Classes
+//
 
 //
 //              This software is the sole property of
