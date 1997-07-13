4990f3ea9338f22769db573d117a9651b1e4e756
houghton <>
1997-07-13 11:09:06 +0000

    Added const walk methods.

--- src/Mdb/DavlTreeOffsetBase.C
+++ src/Mdb/DavlTreeOffsetBase.C
@@ -1,34 +1,65 @@
 //
 // File:        DavlTreeOffsetBase.C
+// Project:	Mdb
 // Desc:        
-//              
 //
-// Author:      Paul Houghton - (houghton@cworld.wiltel.com)
+//  Compiled sources for DavlTreeOffsetBase
+//  
+// Author:      Paul A. Houghton - (paul.houghton@wcom.com)
 // Created:     01/13/95 07:14 
 //
-// Revision History:
+// Revision History: (See end of file for Revision Log)
 //
-// $Log$
-// Revision 2.3  1997/06/19 12:00:46  houghton
-// Changed off_t to Loc
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
 //
-// Revision 2.2  1995/12/04 11:19:17  houghton
-// Changed to eliminate compile warnings.
-//
-// Revision 2.1  1995/11/10  12:42:10  houghton
-// Change to Version 2
-//
-// Revision 1.3  1995/11/05  16:32:28  houghton
-// Revised
-//
-// Revision 1.1  1995/02/13  16:08:36  houghton
-// New Style Avl an memory management. Many New Classes
-//
-//
-const char DavlTreeOffsetBaseRcsId[] =
-"$Id$";
 
 #include "DavlTreeOffsetBase.hh"
+
+#if defined( MDB_DEBUG )
+#include "DavlTreeOffsetBase.ii"
+#endif
+
+MDB_VERSION(
+  DavlTreeOffsetBase,
+  "$Id$");
+
+const char *
+DavlTreeOffsetBase::getClassName( void ) const
+{
+  return( "DavlTreeOffsetBase" );
+}
+
+const char *
+DavlTreeOffsetBase::getVersion( bool withPrjVer ) const
+{
+  return( version.getVer( withPrjVer ) );
+}
+
+ostream &
+DavlTreeOffsetBase::dumpInfo(
+  ostream &	dest,
+  const char *	prefix,
+  bool		showVer
+  ) const
+{
+  if( showVer )
+    dest << DavlTreeOffsetBase::getClassName() << ":\n"
+	 << DavlTreeOffsetBase::getVersion() << '\n';
+
+  // if( ! DavlTreeOffsetBase::good() )
+  //  dest << prefix << "Error: " << DavlTreeOffsetBase::error() << '\n';
+  // else
+  dest << prefix << "Good" << '\n';
+
+
+  return( dest );
+}
+
+DavlTreeOffsetBase::DavlTreeOffsetBase( void )
+{
+}
 
 void
 DavlTreeOffsetBase::insertHist( Loc * firstHist, Loc newHist )
@@ -41,6 +72,43 @@ DavlTreeOffsetBase::insertHist( Loc * firstHist, Loc newHist )
   hist( newHist )->next = *h;
   *h = newHist;
 }
+
+bool
+DavlTreeOffsetBase::walkTree( Loc root, EffDate when )
+{
+  walkWhen = when;
+  walkHist = false;
+
+  return( AvlTreeOffsetBase::walkTree( root ) );
+}
+
+bool
+DavlTreeOffsetBase::walkTree( Loc root, EffDate when, void * closure )
+{
+  walkWhen = when;
+  walkHist = false;
+
+  return( AvlTreeOffsetBase::walkTree( root, closure ) );
+}
+
+bool
+DavlTreeOffsetBase::walkTreeHist( Loc root, EffDate when )
+{
+  walkWhen = when;
+  walkHist = true;
+
+  return( AvlTreeOffsetBase::walkTree( root ) );
+}
+
+bool
+DavlTreeOffsetBase::walkTreeHist( Loc root, EffDate when, void * closure )
+{
+  walkWhen = when;
+  walkHist = true;
+
+  return( AvlTreeOffsetBase::walkTree( root, closure ) );
+}
+
 
 bool
 DavlTreeOffsetBase::walkKeyHist( Loc root, EffDate when )
@@ -197,6 +265,38 @@ DavlTreeOffsetBase::walkNode( Loc root, void * closure )
   return( false );
 }
 
+bool
+DavlTreeOffsetBase::walkNode( Loc root, void * closure ) const
+{
+  for( Loc h = node(root)->hist;
+      h != 0;
+      h = hist( h )->next )
+    {
+      if( hist(h)->when < walkWhen )
+	{
+	  if( walkHist )
+	    {
+	      if( walkAllHistAction( root, h, closure ) )
+		{
+		  return( true );
+		}
+	    }
+	  else
+	    {
+	      if( ! hist(h)->deleted )
+		{
+		  return( walkHistAction( root, h, closure ) );
+		}
+	      else
+		{
+		  return( false );
+		}
+	    }	  
+	}
+    }
+  return( false );
+}
+
 			  
 short
 DavlTreeOffsetBase::trimNode( Loc * root, EffDate when )
@@ -263,7 +363,7 @@ DavlTreeOffsetBase::trimNode( Loc * root, EffDate when )
 		case TREE:
 		{
 		  Loc key 	 = *root;
-		  dec = deleteNode( &( node( *root )->subTree[ RIGHT ] ), &key, -1 );
+		  dec = deleteNode( &( node( *root )->subTree[ RIGHT ] ), 0, &key, -1 );
 
 		  node( key )->subTree[ LEFT ]  = node( *root )->subTree[ LEFT ];
 		  node( key )->subTree[ RIGHT ] = node( *root )->subTree[ RIGHT ];
@@ -366,7 +466,7 @@ DavlTreeOffsetBase::trimNode( Loc * root, EffDate when, void * closure )
 		{
 		  Loc key = *root;
 
-		  dec = deleteNode( &( node( *root )->subTree[ RIGHT ] ), &key, -1 );
+		  dec = deleteNode( &( node( *root )->subTree[ RIGHT ] ), 0, &key, -1 );
 
 		  node( key )->subTree[ LEFT ]  = node( *root )->subTree[ LEFT ];
 		  node( key )->subTree[ RIGHT ] = node( *root )->subTree[ RIGHT ];
@@ -428,6 +528,30 @@ DavlTreeOffsetBase::destroyNode( Loc root, void * closure )
 	  
 	  
     
+
+// Revision Log:
+//
+// $Log$
+// Revision 2.4  1997/07/13 11:09:06  houghton
+// Added const walk methods.
+//
+// Revision 2.3  1997/06/19 12:00:46  houghton
+// Changed off_t to Loc
+//
+// Revision 2.2  1995/12/04 11:19:17  houghton
+// Changed to eliminate compile warnings.
+//
+// Revision 2.1  1995/11/10  12:42:10  houghton
+// Change to Version 2
+//
+// Revision 1.3  1995/11/05  16:32:28  houghton
+// Revised
+//
+// Revision 1.1  1995/02/13  16:08:36  houghton
+// New Style Avl an memory management. Many New Classes
+//
+//
+
   
     
 
