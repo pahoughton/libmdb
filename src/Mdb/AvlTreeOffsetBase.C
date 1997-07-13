2f69d8097286b62197407a2ded40337aa233d4f0
houghton <>
1997-07-13 11:02:07 +0000

    Cleanup.

--- src/Mdb/AvlTreeOffsetBase.C
+++ src/Mdb/AvlTreeOffsetBase.C
@@ -1,20 +1,72 @@
 //
 // File:        AvlTreeOffsetBase.C
+// Project:	Mdb
 // Desc:        
-//              
-//  All the source for the Avl methods is in AvlTreeMethods.INC
+//
+//  Compiled sources for AvlTreeOffsetBase
+//
+//  The source for the Avl methods is in AvlTreeMethods.INC
 //
 //  This was done because both AvlTreeAddrBase and AvlTreeOffsetBase
 //  use the exact same alogrithims. The only differnces are the
 //  ClassName, Node data type, and the fact that AvlTreeOffsetBase needs
-//  to convert a node into an address (a node in that class is an offset).
+//  to convert a node into an address.
 //
-// Author:      Paul Houghton - (houghton@cworld.wiltel.com)
+// Author:      Paul A. Houghton - (paul.houghton@wcom.com)
 // Created:     12/13/94 09:49 
 //
-// Revision History:
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+
+#include "AvlTreeOffsetBase.hh"
+
+#if defined( MDB_DEBUG )
+#include "AvlTreeOffsetBase.ii"
+#endif
+
+MDB_VERSION(
+  AvlTreeOffsetBase,
+  "$Id$");
+
+
+#define ClassName   	AvlTreeOffsetBase
+#define NodeDataType	off_t
+#define NODE( n )   	node( n )
+
+#include "AvlTreeMethods.INC"
+
+AvlTreeOffsetBase::AvlTreeOffsetBase( void )
+{
+}
+
+AvlTreeOffsetBase::AvlTreeOffsetBase(
+  const AvlTreeOffsetBase & CLUE_UNUSED( from )
+  )
+{
+}
+
+const char *
+AvlTreeOffsetBase::getClassName( void ) const
+{
+  return( "AvlTreeOffsetBase" );
+}
+
+const char *
+AvlTreeOffsetBase::getVersion( bool withPrjVer ) const
+{
+  return( version.getVer( withPrjVer ) );
+}
+
+// Revision Log:
 //
 // $Log$
+// Revision 2.3  1997/07/13 11:01:49  houghton
+// Cleanup.
+//
 // Revision 2.2  1995/12/04 11:19:17  houghton
 // Changed to eliminate compile warnings.
 //
@@ -23,22 +75,4 @@
 //
 // Revision 1.1  1995/02/13  16:08:32  houghton
 // New Style Avl an memory management. Many New Classes
-//
-//
-const char AvlTreeOffsetBaseRcsId[] =
-"$Id$";
-
-#include "AvlTreeOffsetBase.hh"
-
-#define ClassName   	AvlTreeOffsetBase
-#define NodeDataType	off_t
-#define NODE( n )   	node( n )
-
-#include "AvlTreeMethods.INC"
-
-//
-// This software is a modificed version of Brad Appleton's
-// 'c' avl library.
-//
-// It is public domain.
 //
--- src/Mdb/AvlTreeOffsetBase.hh
+++ src/Mdb/AvlTreeOffsetBase.hh
@@ -2,9 +2,8 @@
 #define _AvlTreeOffsetBase_hh_
 //
 // File:        AvlTreeOffsetBase.hh
+// Project:	Mdb
 // Desc:        
-//              
-//  NO PUBLIC INTERFACE
 //
 //  This class provides the methods for managing Avl Tress using
 //  offsets instead of direct memory addresses. It can be used in
@@ -16,7 +15,122 @@
 //  template class. It has protected constructors so it can not be
 //  directly instantiated.
 //
-//  DataTypes:
+// Author:      Paul Houghton - (houghton@cworld.wiltel.com)
+//  	    	The source in this class is based on an Avl libraray
+//  	    	(avl.c) written by: Brad Appleton on 03/01/89
+//
+// Created:     12/13/94 09:48
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+//  $Id$
+//
+
+#include <MdbConfig.hh>
+
+#include <AvlTreeBase.hh>
+#include <iostream>
+
+#if defined( MDB_DEBUG )
+#define inline
+#endif
+
+class AvlTreeOffsetBase : public AvlTreeBase
+{
+
+public:
+
+  virtual const char *	getClassName( void ) const;
+  virtual const char *  getVersion( bool withPrjVer = true ) const;
+  
+  static const ClassVersion version;
+
+  // this really should be protected, but the compiler
+  // on AIX barfs!
+  
+  struct Node
+  {
+    Loc	    subTree[2];
+    short   bal;
+  };
+
+protected:
+
+  AvlTreeOffsetBase( void );
+  AvlTreeOffsetBase( const AvlTreeOffsetBase & copyFrom );
+
+  short	    	insertNode( Loc * root, Loc * newNode );
+  Loc		findNode( Loc root, const void * key ) const;
+  short	    	deleteNode( Loc * root,
+			    const void * key,
+			    Loc * delNode,
+			    int minMax = 0);
+
+  void	    	destroyTree( Loc * root );
+  void	    	destroyTree( Loc * root, void * closure );
+
+  bool	    	walkTree( Loc  root );
+  bool	    	walkTree( Loc  root, void * closure );
+
+  bool	    	walkTree( Loc  root, void * closure ) const;
+
+  inline void	initNode( Loc node );
+
+  NodeType  	getNodeType( Loc root );
+
+  short	    	balance( Loc * root );
+
+  // debug/test methods
+  ostream &    	dumpAvlTree( const Loc	root,
+			     ostream &	dest,
+			     int	level = 0 ) const;
+
+  ostream &	dumpNode( ostream & dest, const Loc root, int level ) const;
+  
+  virtual ostream &	dumpKey( ostream & dest, const Loc keyLoc ) const;
+  
+  // pure virtual methods
+  
+  virtual Addr		getBase( void ) = 0;
+  virtual const Addr	getBase( void ) const = 0;
+  
+  virtual int	compareNode( const Loc one, const Loc two) = 0;
+  virtual int	compareFind( const void * one, const Loc two ) const = 0;
+
+  virtual void  destroyNode( Loc root ) = 0;
+  virtual void  destroyNode( Loc root, void * closure ) = 0;
+
+  virtual bool  walkNode( Loc root ) = 0;
+  virtual bool  walkNode( Loc root, void * closure ) = 0;
+  
+  virtual bool  walkNode( Loc root, void * closure ) const = 0;
+  
+private:
+
+  inline Node *     node( Loc root ) const;
+  
+  void		    rotateTwice( Loc * root, SubTree dir );
+  short	    	    rotateOnce( Loc * root, SubTree dir );
+
+};
+
+#if !defined( inline )
+#include <AvlTreeOffsetBase.ii>
+#else
+#undef inline
+
+
+#endif
+
+
+//
+// Detail Documentation
+//
+//  Data Types: - data types defined by this header
 //
 //  	AvlTreeOffsetBase    	class
 //  	AvlTreeOffsetBase::Node	a single avl tree node.
@@ -32,23 +146,36 @@
 //  	    need to do anything. It defined to prevent
 //  	    direct instanciation.
 //
+//  Public Interface:
+//
+//  	virtual const char *
+//  	getClassName( void ) const;
+//  	    Return the name of this class (i.e. AvlTreeOffsetBase )
+//
+//  	virtual const char *
+//  	getVersion( bool withPrjVer = true ) const;
+//  	    Return the version string of this class.
+//
+//	static const ClassVersion version
+//	    Class and project version information. (see ClassVersion.hh)
+//
 //  Protected Interface:
 //
 //  	short
-//  	insertNode( off_t * root, off_t * newNode );
+//  	insertNode( Loc * root, Loc * newNode );
 //  	    Insert a new node into the tree. If 'newNode' is a
 //  	    duplicate node (see compare), 'newNode' will contain
 //  	    the existing node on return.
 //  	    Returns the change in tree balance cause by inserting
 //  	    the new node.
 //
-//  	off_t
-//  	findNode( off_t root, off_t key );
+//  	Loc
+//  	findNode( Loc root, Loc key );
 //  	    Search the tree for a node that matches key.
 //  	    Returns the address of the found node or 0 if not found.
 //
 //  	short
-//  	deleteNode( off_t * root, off_t * key, int minMax = 0 );
+//  	deleteNode( Loc * root, Loc * key, int minMax = 0 );
 //  	    Delete the node from the tree that matches 'key'. If 'minMax'
 //  	    is > 0, the value of 'key' is ignored and the largest
 //  	    (right most) node is deleted. If 'minMax' is < 0, the value
@@ -57,55 +184,102 @@
 //  	    no match is found.
 //  	    Returns the change in tree balance caused by deleting the node.
 //
+//  	void
+//  	destroyTree( Loc * root );
+//  	    Remove all nodes from the tree. A user function is call
+//  	    at each node so the data tied to the node can be deleted.
+//
+//  	void
+//  	destroyTree( Loc * root, void * closure );
+//  	    Remove all nodes from the tree. A user function is call
+//  	    at each node so the data tied to the node can be deleted.
+//  	    'closure' is passed to the user function.
+//
 //  	bool
-//  	walkTree( off_t root );
+//  	walkTree( Loc root );
 //  	    Walk the entire tree in accending order (left to right) and
 //  	    execute a user function at each node.
 //  	    Returns FALSE if the entire tree was walked. TRUE
 //  	    will be returned if the walk was aborted buy 'walkNode'
 //
 //  	bool
-//  	walkTree( off_t root, void * closure );
+//  	walkTree( Loc root, void * closure );
 //  	    Walk the entire tree in accending order (left to right) and
 //  	    execute a user function at each node. 'closure' is passed
 //  	    to the user function.
 //  	    Returns FALSE if the entire tree was walked. TRUE
 //  	    will be returned if the walk was aborted buy 'walkNode'
 //
+//	inline
 //  	void
-//  	initNode( off_t node );
+//  	initNode( Loc node );
 //  	    Initialize a new node. Sets all values to 0.
 //
+//  	NodeType
+//  	getNodeType( Loc node );
+//  	    Return the type of 'node'.
+//
+//  	short
+//  	balance( Loc * root );
+//  	    Re-balance the subTree at 'root'.
+//  	    Returns the change in balance.
+//
 //  	ostream &
-//  	dumpTree( const off_t root, ostream & dest, int level = 0 ) const;
+//  	dumpAvlTree( const Loc root, ostream & dest, int level = 0 ) const;
 //  	    This method will output a 'graph' of the tree structure.
 //  	    The graph show each node's position and balance in the tree.
 //  	    Returns the ostream passed as 'dest'
 //
-//  	void
-//  	destroyTree( off_t * root );
-//  	    Remove all nodes from the tree. A user function is call
-//  	    at each node so the data tied to the node can be deleted.
+//	ostream &
+//	dumpNode( ostream & dest, const Loc root, int level ) const
+//	    Output a single node of the three for 'dumpTree()'.
+//  	    Returns the ostream passed as 'dest'
 //
-//  	void
-//  	destroyTree( off_t * root, void * closure );
-//  	    Remove all nodes from the tree. A user function is call
-//  	    at each node so the data tied to the node can be deleted.
-//  	    'closure' is passed to the user function.
+//	virtual
+//	ostream &
+//	dumpKey( ostream & dest, const Loc keyLoc ) const = 0
+//	    This virtual should output the key value to 'dest'.
+//	    It is called by dumpNode. If it is not implemented by a sub
+//	    class, no key will be output.
+//  	    Returns the ostream passed as 'dest'
+//	
+//  Protected Pure Virtual Methods:
+//
+//	virtual
+//	Addr
+//	getBase( void ) = 0
+//	    This pure virtual needs to return the base address for the
+//	    offsets (Loc) used thoughout the tree.
+//
+//	virtual
+//	const Addr
+//	getBase( void ) const = 0
+//	    This pure virtual needs to return the base address for the
+//	    offsets (Loc) used thoughout the tree.
 //
 //  	virutal
 //  	int
-//  	compare( const off_t one, const off_t two ) = 0;
-//  	    This pure virtual function must be defined by all sub classes.
+//  	compareNode( const Loc one, const Loc two ) = 0;
+//  	    This pure virtual function must be defined by any sub classes.
 //  	    It is used to determine difference between the two nodes
 //  	    passed to it.
 //  	    Returns > 0 if node 'one' is greater than node 'two', = 0 if
 //  	    node 'one' and 'two' are equal and < 0 if node 'one' is less
 //  	    than node two.
 //
+//  	virutal
+//  	int
+//  	compareFind( const void * one, const Loc two ) = 0;
+//  	    This pure virtual function must be defined by any sub classes.
+//  	    It is used to determine difference between a key value and a
+//	    node in the tree.
+//  	    Returns > 0 if node 'one' is greater than node 'two', = 0 if
+//  	    node 'one' and 'two' are equal and < 0 if node 'one' is less
+//  	    than node two.
+//
 //  	vitual
 //  	void
-//  	destroyNode( off_t node ) = 0;
+//  	destroyNode( Loc node ) = 0;
 //  	    This pure virtual function must be defined by all sub classes.
 //  	    It is called for each node from within 'destroyTree'. It should
 //  	    release all memory used by the node including the 'node' it
@@ -113,7 +287,7 @@
 //  	    
 //  	vitual
 //  	void
-//  	destroyNode( off_t node, void * closure ) = 0;
+//  	destroyNode( Loc node, void * closure ) = 0;
 //  	    This pure virtual function must be defined by all sub classes.
 //  	    It is called for each node from within 'destroyTree'. It should
 //  	    release all memory used by the node including the 'node' it
@@ -121,7 +295,7 @@
 //
 //  	vitual
 //  	bool
-//  	walkNode( off_t node ) = 0;
+//  	walkNode( Loc node ) = 0;
 //  	    This pure virtual function must be defined by all sub classes.
 //  	    'walkNode' is called by 'walkTree' for each node in the tree.
 //  	    Return FALSE to continue to the next node in the tree.
@@ -129,44 +303,40 @@
 //  	    
 //  	vitual
 //  	bool
-//  	walkNode( off_t node, void * closure ) = 0;
+//  	walkNode( Loc node, void * closure ) = 0;
 //  	    This pure virtual function must be defined by all sub classes.
 //  	    'walkNode' is called by 'walkTree' for each node in the
 //  	    tree.'closure' is the value passed as closure to 'walkTree'.
 //  	    Return FALSE to continue to the next node in the tree.
 //  	    Return TRUE if you want to abort the walk.
 //  	    
-//  	NodeType
-//  	getNodeType( off_t node );
-//  	    Return the type of 'node'.
-//
-//  	short
-//  	balance( off_t * root );
-//  	    Re-balance the subTree at 'root'.
-//  	    Returns the change in balance.
-//
-//
-//  Protected Data:
-//
-//  	baseAddr    the current base address for all offsets
-//
 //  Private Methods:
 //
 //  	Node *
-//  	node( off_t root );
+//  	node( Loc root );
 //  	    convert an offset to a Node *;
 //
 //  	void
-//  	rotateTwice( off_t * root, SubTree dir );
+//  	rotateTwice( Loc * root, SubTree dir );
 //  	    This method is called by balance to rotate the nodes twice
 //  	    and balance the tree. For a more detailed explination,
 //  	    see the comments in AvlTreeMethods.INC.
 //
 //  	short
-//  	rotateOnce( off_t * root, SubTree dir );
+//  	rotateOnce( Loc * root, SubTree dir );
 //  	    This methode is called by balance to rotate the node and
 //  	    balance the tree. For a more detailed explination,
 //  	    see the comments in AvlTreeMethods.INC.
+//
+//  Private Methods:
+//
+//  Associated Functions:
+//
+//  	ostream &
+//  	operator <<( ostream & dest, const AvlTreeOffsetBase & src );
+//
+//	istream &
+//	operator >> ( istream & src, AvlTreeOffsetBase & dest );
 //
 // Notes:
 //
@@ -174,16 +344,29 @@
 //    G.M. Adelson-Velskii and E.M. Landis. Doklady Akademia Nauk SSSR, 146,
 //    (1962), 263-66; English translation in Soviet Math, 3, 1259-63
 //
-// Author:      Paul Houghton - (houghton@cworld.wiltel.com)
-//  	    	The source in this class is based on an Avl libraray
-//  	    	(avl.c) written by: Brad Appleton on 03/01/89
+// Example:
 //
-// Created:     12/13/94 09:48
+// See Also:
 //
-// Revision History:
+// Files:
 //
-// 
+// Documented Ver:
+//
+// Tested Ver:
+//
+// Copyright:
+//
+//	This software is a modificed version of Brad Appleton's
+//	'c' avl library.
+//
+//	It is public domain.
+//
+// Revision Log:
+//
 // $Log$
+// Revision 2.4  1997/07/13 11:02:07  houghton
+// Cleanup.
+//
 // Revision 2.3  1997/06/25 12:50:37  houghton
 // Removed typedef Loc (now in AvlTreeBase).
 // cleanup.
@@ -201,117 +384,4 @@
 // New Style Avl an memory management. Many New Classes
 //
 //
-
-#include <MdbConfig.hh>
-
-#include <AvlTreeBase.hh>
-#include <sys/types.h>
-#include <iostream.h>
-
-
-
-class AvlTreeOffsetBase : public AvlTreeBase
-{
-
-public:
-
-  
-protected:
-
-public:
-  // this really should be protected, but the compiler
-  // on AIX barfs!
-  
-  struct Node
-  {
-    Loc	    subTree[2];
-    short    bal;
-  };
-
-protected:
-
-  AvlTreeOffsetBase( void );
-  AvlTreeOffsetBase( const AvlTreeOffsetBase & copyFrom );
-
-  virtual caddr_t   	getBase( void ) = 0;
-  virtual const caddr_t getBase( void ) const = 0;
-  
-  short	    	insertNode( Loc * root, Loc * newNode );
-  Loc		findNode( Loc root, const void * key );
-  short	    	deleteNode( Loc * root, Loc * key, int minMax = 0);
-
-  bool	    	walkTree( Loc  root );
-  bool	    	walkTree( Loc  root, void * closure );
-
-  void	    	initNode( Loc node );
-  
-  ostream &    	dumpTree( const Loc root,
-			  ostream & dest,
-			  int level = 0 ) const;
-
-  ostream &	dumpNode( ostream & dest, const Loc root, int level ) const;
-  
-  virtual ostream & dumpKey( ostream & dest, const Loc keyLoc ) const = 0;
-  
-  void	    	destroyTree( Loc * root );
-  void	    	destroyTree( Loc * root, void * closure );
-
-  virtual int	compareNode( const Loc one, const Loc two) = 0;
-  virtual int	compareFind( const void * one, const Loc two ) = 0;
-
-  virtual void  destroyNode( Loc root ) = 0;
-  virtual void  destroyNode( Loc root, void * closure ) = 0;
-
-  virtual bool  walkNode( Loc root ) = 0;
-  virtual bool  walkNode( Loc root, void * closure ) = 0;
-  
-  NodeType  	    getNodeType( Loc  root );
-
-  short	    	    balance( Loc * 	root );
-
-private:
-
-  Node *    	    node( Loc root ) const;
-  
-  void	    	    rotateTwice( Loc * root,
-				 SubTree   	dir );
-
-  short	    	    rotateOnce( Loc * 	root,
-				SubTree    	dir );
-
-};
-
-
-//
-// Inline methods
-//
-
-inline
-AvlTreeOffsetBase::AvlTreeOffsetBase( void )
-{
-}
-
-inline
-AvlTreeOffsetBase::Node *
-AvlTreeOffsetBase::node( Loc root ) const
-{
-  return( (Node *)( getBase() + root ) );
-}
-
-
-inline
-void
-AvlTreeOffsetBase::initNode( Loc root )
-{
-  node( root )->subTree[ LEFT ]     = 0;
-  node( root )->subTree[ RIGHT ]    = 0;
-  node( root )->bal 	    	    = 0;
-}
-
 #endif // ! def _AvlTreeOffsetBase_hh_ 
-//
-// This software is a modificed version of Brad Appleton's
-// 'c' avl library.
-//
-// It is public domain.
-//
