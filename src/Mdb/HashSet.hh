af957c2c3f09f4ad7bcb720299561f61b6b7d013
houghton <>
1997-07-13 11:09:59 +0000

    Cleanup. Finished coding.

--- src/Mdb/HashSet.hh
+++ src/Mdb/HashSet.hh
@@ -5,9 +5,11 @@
 // Project:	Mdb
 // Desc:        
 //
+//  This template class provides a mapped memory Hash index for a single
+//  data type (usually a struct). It stores the hash index in a
+//  MapFile (indexFile) and the data in the MultiMemOffset passed to it.
 //
-//
-// Quick Start: - short example of class usage
+//  Most of the functionallity provided comes from the HashTable class.
 //
 // Author:      Paul A. Houghton - (paul.houghton@wcom.com)
 // Created:     06/02/97 05:13
@@ -22,12 +24,10 @@
 //
 
 #include <MdbConfig.hh>
+#include <MdbUtils.hh>
 #include <HashTable.hh>
+#include <DumpInfo.hh>
 #include <iostream>
-
-#if defined( MDB_DEBUG )
-#define inline
-#endif
 
 template< class Key,
 	  class HashFunct,
@@ -37,75 +37,281 @@ class Hash
 
 public:
 
-  typedef HashTable< Key, Key, ident< Key, Key >, HashFunct, LessKey > Table;
-  typedef Table::const_iterator	const_iterator;
+  typedef HashTable< Key, Key, MdbIdent< Key, Key >, HashFunct, LessKey > Table;
+  typedef Table::size_type		size_type;
+  typedef Table::iterator		iterator;
+  typedef Table::const_iterator		const_iterator;
+  typedef Table::reverse_iterator	reverse_iterator;
+  typedef Table::const_reverse_iterator	const_reverse_iterator;
+  typedef Table::pair_iterator_bool	pair_iterator_bool;
   
-  inline Hash( ChunkMgr &	chunkMgr,
+  inline Hash( MultiMemOffset *	memMgr,
 	       const char *	indexFileName,
 	       ios::open_mode	mode = ios::in,
-	       unsigned short	permMask = 0,
-	       bool		create = false );
+	       bool		create = false,
+	       unsigned short	permMask = 02 );
   
   virtual ~Hash( void );
 
+  inline pair_iterator_bool	insert( const Key & key ) {
+    return( table.insert( key ) );
+  };
+  
+  inline iterator		find( const Key & key ) {
+    return( table.find( key ) );
+  };
+
+  inline const_iterator		find( const Key & key ) const {
+    return( table.find( key ) );
+  };
+  
+  inline bool			erase( const Key & key ) {
+    return( table.erase( key ) );
+  };
+
+  inline bool			erase( const iterator & pos ) {
+    return( table.erase( pos ) );
+  };
+
+  inline bool			erase( const iterator & first,
+				       const iterator & last ) {
+    return( table.erase( first, last ) );
+  };
+  
+  inline iterator	    begin( void ) { return( table.begin() ); };
+  inline iterator	    end( void ) { return( table.end() ); };
+  
+  inline const_iterator	    begin( void ) const { return( table.begin() ); };
+  inline const_iterator	    end( void ) const { return( table.end() ); };
+
+  inline reverse_iterator	rbegin( void ) { return( table.rbegin() ); };
+  inline reverse_iterator	rend( void ) { return( table.rend() ); };
+
+  inline const_reverse_iterator	rbegin( void ) const {return(table.rbegin());};
+  inline const_reverse_iterator	rend( void ) const {return(table.rend());};
+
+  inline size_type	    size( void ) const { return( table.size() ); };
+  inline bool		    empty( void ) const { return( table.empty() ); };
+
+  static size_type	    getNodeSize( void ) {
+    return( Table::getNodeSize() );
+  };
+  
+  virtual bool		good( void ) const;
+  virtual const char *	error( void ) const;
+  virtual const char *	getClassName( void ) const;
+  virtual const char *  getVersion( bool withPrjVer = true ) const;
+  virtual ostream &     dumpInfo( ostream &	dest = cerr,
+				  const char *  prefix = "    ",
+                                  bool          showVer = true ) const;
+
+  inline
+  DumpInfo< Hash< Key, HashFunct, LessKey > >
+  dump( const char *  prefix = "    ",
+	bool	    showVer = true ) const {
+    return( DumpInfo< Hash< Key, HashFunct, LessKey> >(
+      *this, prefix, showVer ) );
+  };
+
+  // Debug methods
+  inline ostream &	dumpTable( ostream & dest ) const;
+
+  virtual ostream &	dumpKey( ostream & dest, const Key & key ) const;
+  virtual ostream &	dumpValue( ostream & dest, const Key & value ) const;
+
+  class HashDumpMethods : public Table::TableDumpMethods
+  {
+  public:
+    HashDumpMethods( const Table & table,
+		     const Hash< Key, HashFunct, LessKey > & me ) :
+      Table::TableDumpMethods( table ), self( me ) {};
+
+    ostream &  dumpKey( ostream & dest, const Key & key ) const {
+      return( self.dumpKey( dest, key ) );
+    };
+    
+    ostream &  dumpValue( ostream & dest, const Key & value ) const {
+      return( self.dumpValue( dest, value ) );
+    };
+
+  private:
+    const Hash< Key, HashFunct, LessKey > & self;
+    
+  };
   
 protected:
 
 private:
 
-  Hash( const Hash & from );
-  Hash & operator =( const Hash & from );
+  // HashTable will prevent these
+  //
+  // Hash( const Hash & from );
+  // Hash & operator =( const Hash & from );
 
+  Table		table;
+  
 };
 
-#if !defined( inline )
 #include <Hash.ii>
-#else
-#undef inline
-
-ostream &
-operator << ( ostream & dest, const Hash & src );
-
-istream &
-operator >> ( istream & src, const Hash & dest );
-
-
-#endif
 
 
 //
 // Detail Documentation
 //
-//  Data Types: - data types defined by this header
+//  Data Types:
 //
-//  	Hash	class
+//  	Hash< Key, HashFunct, LessKey >:	    template class
+//
+//	    This is the primary template class defined by this header.
+//	    It uses the following three types:
+//
+//	    'Key' is the data type being indexed and stored. It should
+//		be struct. It must not contain a virtual table.
+//
+//	    'HashFunct' is a class that returns a 'HashTableBase::Hash'
+//		for a 'Key' value.
+//
+//	    'LessKey' is a class that takes two 'Key' values and compares
+//		them. If the first is less that the second, true should
+//		be returned.
+//
+//	Table:
+//	    This uses the HashTable template class to provide most of the
+//	    functionallity.
+//
+//	size_type:		used for size values.
+//	iterator:		a bidirectional iterator.
+//	const_iterator:		a const bidirectional iterator.
+//	reverse_iterator:	a bidirectional iterator.
+//	const_revers_iterator:	a const bidirectional iterator.
+//	pair_iterator_bool:	an iterator and a bool value.
 //
 //  Constructors:
 //
-//  	Hash( );
+//  	Hash( MultiMemOffset *	    memMgr,
+//	      const char *	    indexFileName,
+//	      ios::open_mode	    mode = ios::in,
+//	      bool		    create = false,
+//	      unsigned short	    permMask = 02 )
+//	    Construct a 'Hash' object that will either use existing
+//	    data maps or create empty ones.
+//
+//		'memMgr' is the object used to store and manage the
+//		    'Key' ojects.
+//		'indexFileName' is the name of the MapFile to use for
+//		    the Hash index.
+//		'mode' is the mode for the index, ios::in == read only
+//		    (ios::in | ios::out) == read/write (note if create == true
+//		    the mode argument is ignored and the file is opened
+//		    with (ios::in | ios::out) ).
+//		'create' if this is true, a new empty index will be created.
+//		    Any existing file will be removed.
+//		'permMask' is the 'umask(2)' to use when creating the
+//		    index file. A value of '02' will create a file with
+//		    the mode set to '-rw-rw-r--'.
 //
 //  Destructors:
 //
+//	~Hash()
+//	    The destructor will close the indexFile, but it does NOT
+//	    delete (or do anything else with) the 'memMgr'.
+//
 //  Public Interface:
 //
-//	virtual ostream &
-//	write( ostream & dest ) const;
-//	    write the data for this class in binary form to the ostream.
+//	inline
+//	pair_iterator_bool
+//	insert( cosnt Key & key )
+//	    Insert a new 'key' into the hash table. Duplicate keys
+//	    (according to LessKey) are inserted. On return,
+//	    pair_iterator_bool.second will be true if the value was
+//	    inserted. pair_iterator_bool.first will contain a iterator
+//	    for the inserted value. 
 //
-//	virtual istream &
-//	read( istream & src );
-//	    read the data in binary form from the istream. It is
-//	    assumed it stream is correctly posistioned and the data
-//	    was written to the istream with 'write( ostream & )'
+//	inline
+//	iterator
+//	find( const Key & key )
+//	    Find 'key' in the hash table and return an iterator to it.
+//	    If 'key' is not found, end() is returned.
 //
-//	virtual ostream &
-//	toStream( ostream & dest ) const;
-//	    output class as a string to dest (used by operator <<)
+//	inline
+//	const_iterator
+//	find( const Key & key ) const
+//	    Find 'key' in the hash table and return a const_iterator to it.
+//	    If 'key' is not found, end() is returned.
 //
-//	virtual istream &
-//	fromStream( istream & src );
-//	    Set this class be reading a string representation from
-//	    src. Returns src.
+//	inline
+//	bool
+//	erase( const Key & key )
+//	    Removes the value. 'true' is returned if the value is removed.
+//
+//	inline
+//	bool
+//	erase( const iterator & pos )
+//	    Removes the value at 'pos'. 'true' is returned if the value is
+//	    removed.
+//
+//	inline
+//	bool
+//	erase( const iterator & first, const iterator & last )
+//	    Removes the first through last values. 'first' is removed and
+//	    'last' is not removed. Returns 'true' if the values were
+//	    removed.
+//
+//	inline
+//	iterator
+//	begin( void )
+//	    Returns an iterator to the first value in the table.
+//
+//	inline
+//	iterator
+//	end( void )
+//	    Returns an invalid iterator that should be used to verify the
+//	    iterator returned by find and when incrementing the iterator
+//	    returned by begin.
+//
+//	inline
+//	const_iterator
+//	begin( void ) const
+//	    Returns a const_iterator to the first value in the table.
+//
+//	inline
+//	const_iterator
+//	end( void ) const
+//	    Returns an invalid iterator that should be used to verify the
+//	    iterator returned by find and when incrementing the iterator
+//	    returned by begin.
+//
+//	inline
+//	iterator
+//	rbegin( void )
+//	    Returns an iterator to the last value in the table.
+//
+//	inline
+//	iterator
+//	rend( void )
+//	    Returns an invalid iterator that should be used to verify the
+//	    iterator when incrementing the iterator returned by rbegin.
+//
+//	inline
+//	const_iterator
+//	rbegin( void ) const
+//	    Returns an iterator to the last value in the table.
+//
+//	inline
+//	const_iterator
+//	rend( void ) const
+//	    Returns an invalid iterator that should be used to verify the
+//	    iterator when incrementing the iterator returned by rbegin.
+//
+//	inline
+//	size_type
+//	size( void ) const
+//	    Return the number of entries in the table.
+//
+//	inline
+//	bool
+//	empty( void ) const
+//	    Returns true if size() == 0.
 //
 //  	virtual Bool
 //  	good( void ) const;
@@ -125,12 +331,32 @@ operator >> ( istream & src, const Hash & dest );
 //  	    Return the version string of this class.
 //
 //	virtual ostream &
-//	dumpInfo( ostream & dest, const char * prefix, bool showVer );
+//	dumpInfo( ostream & dest, const char * prefix, bool showVer ) const
 //	    output detail info to dest. Includes instance variable
 //	    values, state info & version info.
 //
-//	static const ClassVersion version
-//	    Class and project version information. (see ClassVersion.hh)
+//	inline
+//	DumpInfo< MapMemDynamicDynamic >
+//	dump( const char * previx, bool showVer = true ) const
+//	    return an object that can be passed to operator << ( ostream & )
+//	    that will generate the same output as 'dumpInfo'.
+//
+//	inline
+//	ostream &
+//	dumpTable( ostream & dest ) const
+//	    Outputs the structure of the table to 'dest'.
+//
+//	inline
+//	ostream &
+//	dumpKey( ostream & dest, const Key & key ) const
+//	    Override this method in a sub class to output the key value(s)
+//	    with the table structure output by dumpTable.
+//
+//	inline
+//	ostream &
+//	dumpValue( ostream & dest, const Key & key ) const
+//	    Override this method in a sub class to output the
+//	    non-key values with the table structure output by dumpTable.
 //
 //  Protected Interface:
 //
@@ -138,25 +364,65 @@ operator >> ( istream & src, const Hash & dest );
 //
 //  Associated Functions:
 //
-//  	ostream &
-//  	operator <<( ostream & dest, const Hash & src );
-//
-//	istream &
-//	operator >> ( istream & src, Hash & dest );
-//
 // Example:
+//
+//	struct ExRec
+//	{
+//	  long	key;	    
+//	  short	subKey;	    
+//	  char	data[16];
+//	};
+//	
+//	struct ExHashFunct
+//	{
+//	  HashTableBase::Hash	operator () ( const ExRec & rec ) const {
+//	    return( rec.key );
+//	  };
+//	};
+//	
+//	struct ExLessKey
+//	{
+//	  bool	    operator () ( const ExRec & one,
+//				  const ExRec & two ) const {
+//	    return( one.key == two.key ?
+//		    one.subKey < two.subKey :
+//		    one.key < two.key );
+//	  };
+//	};
+//	
+//	typedef Hash< ExRec, ExHashFunct, ExLessKey >	ExTable;
+//
 //
 // See Also:
 //
+//	HashTable(3), HashTableBase(3),
+//	Dash(3), DashTable(3), DashTableBase(3),
+//	MapFile(3), MultiMemOffset(3), MultiMemOffsetMapDynamic(3),
+//	MapMemDynamicDynamic(3), MapMemDynamicFixed(3)
+//
+//	libMdb/docs/design/HashTable.txt
+//	libMdb/docs/design/HashTableBase.txt
+// 
 // Files:
 //
-// Documented Ver:
+//	Hash.hh, Hash.ii,
+//	HashTable.hh, HashTable.ii,
+//	HashTableBase.hh, HashTableBase.ii,
+//	MapFile.hh, MapFile.ii,
+//	MutliMemOffset.hh, MultiMemOffset.ii
+//	libMdb.a
 //
-// Tested Ver:
+// Documented Ver: 2.2
+//
+// Tested Ver: 2.2
 //
 // Revision Log:
 //
 // $Log$
+// Revision 2.2  1997/07/13 11:09:59  houghton
+// Cleanup.
+// Finished coding.
+//
 // Revision 2.1  1997/06/05 11:29:10  houghton
 // Initial Version.
 //
