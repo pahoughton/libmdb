b8942a867601b75db8b3bd9a4af17dbec129ad94
houghton <>
1997-07-14 10:51:17 +0000

    Added detail info output to AppDebug.

--- test/Perf/libPerf/LogPerfData.C
+++ test/Perf/libPerf/LogPerfData.C
@@ -15,9 +15,12 @@
 //  Version:	    $Revision$
 //
 
+#include <AppParam.hh>
+
 #include <TimeIt.hh>
 #include <iostream>
 #include <iomanip>
+
 void
 LogPerfData(
   ostream &	    dest,
@@ -31,6 +34,7 @@ LogPerfData(
   const TimeIt &    timer
   )
 {
+  dest.setf( ios::left, ios::adjustfield );
   dest << setw(20) << mapType
        << " "
        << setw(20) << className
@@ -54,12 +58,42 @@ LogPerfData(
   dest.setf( ios::internal, ios::adjustfield );
   dest << timer
        << endl;
+
+  AppDebug;
+
+  App->log().setf( ios::left, ios::adjustfield );
+  App->log() << setw(20) << mapType
+	     << " "
+	     << setw(20) << className
+	     << " "
+    ;
+  App->log().setf( ios::internal, ios::adjustfield );
+  App->log() << setw( 5 ) << initAlloc
+	     << " "
+	     << setw( 4 ) << recSize
+	     << " "
+	     << setw( 8 ) << quantity
+	     << " "
+    ;
+  App->log().setf( ios::left, ios::adjustfield );
+  App->log() << setw(10) << method
+	     << " "
+	     << setw(5) <<  order
+	     << " "
+    ;
+  App->log().setf( ios::internal, ios::adjustfield );
+  App->log() << timer.dump()
+	     << endl;
+
 }
 
 
 // Revision Log:
 //
 // $Log$
+// Revision 1.2  1997/07/14 10:51:17  houghton
+// Added detail info output to AppDebug.
+//
 // Revision 1.1  1997/07/13 11:36:39  houghton
 // Initial Version.
 //
