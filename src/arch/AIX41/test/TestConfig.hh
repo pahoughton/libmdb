c8a74a9534eedd391dc97c9959cc5418f1dd5b5d
houghton <>
1997-07-14 10:55:23 +0000

    Initial Version.

---
+++ src/arch/AIX41/test/TestConfig.hh
@@ -0,0 +1,35 @@
+#ifndef _TestConfig_hh_
+#define _TestConfig_hh_
+//
+// File:        TestConfig.hh
+// Project:	Mdb
+// Desc:        
+//
+//
+//
+// Quick Start: - short example of class usage
+//
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     07/13/97 06:42
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+//  $Id$
+//
+
+#include <MdbConfig.hh>
+
+//
+// Revision Log:
+//
+// $Log$
+// Revision 1.1  1997/07/14 10:55:23  houghton
+// Initial Version.
+//
+//
+#endif // ! def _TestConfig_hh_ 
+
