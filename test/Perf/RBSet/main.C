57bc8cfb5a21d5a087eb88267022942fead8468e
houghton <>
1997-07-25 13:52:47 +0000

    Reworked for new style performance test.

--- test/Perf/RBSet/main.C
+++ test/Perf/RBSet/main.C
@@ -19,10 +19,6 @@
 
 #include "MdbConfig.hh"
 #include "AppParam.hh"
-#include "PerfRBSet.hh"
-#include <MapMemDynamicDynamic.hh>
-#include <MapMemDynamicFixed.hh>
-#include <MultiMemOffsetMapDynamic.hh>
 #include <iostream>
 
 AppParam * App = 0;
@@ -30,6 +26,11 @@ AppParam * App = 0;
 MDB_FUNCT_VERSION(
   main,
   "$Id$");
+
+bool PerfTable_4( ostream & perfDest );
+bool PerfTable_128( ostream & perfDest );
+bool PerfTable_512( ostream & perfDest );
+bool PerfTable_1024( ostream & perfDest );
 
 int
 main( int argc, char * argv[] )
@@ -45,47 +46,28 @@ main( int argc, char * argv[] )
     App->abort( 1, true, __FILE__, __LINE__ );
 
   ofstream  perfDest( App->perfInfoFn(), ios::out|ios::app, 0664 );
-  
-  MapMemDynamic	* mmd = 0;
-  
-  if( App->mapType() == "mdd" )
-    mmd = new MapMemDynamicDynamic( App->tableFn(),
-				    App->tableMode(),
-				    App->create(),
-				    Table_4::getNodeSize(),
-				    App->allocSize(),
-				    02 );
 
-  if( App->mapType() == "mdf" )
-    mmd = new MapMemDynamicFixed( App->tableFn(),
-				  App->tableMode(),
-				  App->create(),
-				  Table_4::getNodeSize(),
-				  App->allocSize(),
-				  02 );
-
-  if( ! mmd )
+  switch( App->recSize() )
     {
-      AppError << "bad map type: " << App->mapType() << endl;
-      exit( 1 );
-    }
+    case 4:
+      return( PerfTable_4( perfDest ) ? 0 : 1 );
 
-  if( ! mmd->good() )
-    {
-      AppError << mmd->error() << endl;
-      exit( 1 );
+    case 128:
+      return( PerfTable_128( perfDest ) ? 0 : 1 );
+      
+    case 512:
+      return( PerfTable_512( perfDest ) ? 0 : 1 );
+      
+    case 1024:
+      return( PerfTable_1024( perfDest ) ? 0 : 1 );
+
+    default:
+      AppError << "Unsupported record size: " << App->recSize() << endl;
+      return( 0 );
     }
   
-  MultiMemOffsetMapDynamic  mmo( mmd, true );
-
-  if( ! mmo.good() )
-    {
-      AppError << mmo.error() << endl;
-      exit( 1 );
-    }
-  
-  Table_4   table( &mmo, 0, App->create() );
-
+      
+#if defined( OLD_WAY )  
   if( ! table.good() )
     {
       AppError << table.error() << endl;
@@ -97,81 +79,6 @@ main( int argc, char * argv[] )
   
   if( App->insert() )
     {
-      long first;
-      long last;
-      
-      if( App->order() == "asc" )
-	{
-	  first = table.size();
-	  last = first + App->qty();
-	}
-      else
-	{
-	  if( App->order() == "rev" )
-	    {
-	      last = table.size();
-	      first = last + App->qty();
-	    }
-	  else
-	    {
-	      AppError << "unsupported order: " << App->order() << endl;
-	      exit( 1 );
-	    }
-	}
-      
-      if( first > last )
-	{
-	  Rec_4 r;
-	  timer.start();
-	  for( long k = first; k < last ; ++ k )
-	    {
-	      r.k = k;
-	      if( ! table.insert( r ).second )
-		{
-		  AppError << "insert '" << k << "' failed." << endl;
-		  exit( 1 );
-		}
-	    }
-
-	  timer.stop();
-
-	  LogPerfData( perfDest,
-		       App->mapType(),
-		       table.getClassName(),
-		       "insert",
-		       App->order(),
-		       App->allocSize(),
-		       Table_4::getNodeSize(),
-		       App->qty(),
-		       timer );
-	}
-      else
-	{
-	  Rec_4 r;
-	  timer.start();
-	  for( long k = last - 1; k >= first ; -- k )
-	    {
-	      r.k = k;
-	      if( ! table.insert( r ).second )
-		{
-		  AppError << "insert '" << k << "' failed." << endl;
-		  exit( 1 );
-		}
-	    }
-
-	  timer.stop();
-
-	  LogPerfData( perfDest,
-		       App->mapType(),
-		       table.getClassName(),
-		       "insert",
-		       App->order(),
-		       App->allocSize(),
-		       Table_4::getNodeSize(),
-		       App->qty(),
-		       timer );
-	}
-    }
 
   if( App->find() )
     {
@@ -204,13 +111,17 @@ main( int argc, char * argv[] )
 		   timer );
     }
 
-  AppDebug << table.dump() << endl;
   return( 0 );
+#endif
+  
 }
 //
 // Revision Log:
 //
 // $Log$
+// Revision 1.2  1997/07/25 13:52:47  houghton
+// Reworked for new style performance test.
+//
 // Revision 1.1  1997/07/21 10:29:41  houghton
 // Initial version.
 //
